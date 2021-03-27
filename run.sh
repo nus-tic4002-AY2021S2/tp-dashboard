4ef1ee120fb69e78fe203cf71e030be85c5e50f7
Damith C. Rajapakse <damith@gmail.com>
2021-03-27 11:31:20 +0800

    use release

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
-./get-reposense.py --master
+./get-reposense.py --release
 java -jar RepoSense.jar --config ./configs --since 12/02/2021 --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
