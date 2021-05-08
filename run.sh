c952cd497c5b99656dca7b248f7b84eb12f6d1b5
Damith C. Rajapakse <damith@gmail.com>
2021-05-08 13:39:54 +0800

    Add until date

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 ./get-reposense.py --release
-java -jar RepoSense.jar --config ./configs --since 12/02/2021 --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
+java -jar RepoSense.jar --config ./configs --since 12/02/2021 --until 01/05/2021 --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
